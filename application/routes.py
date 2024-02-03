4740088ad6cb76d2c43e489044e71ba49fd41513
Anders Bjorland <67209257+CallMeRubb@users.noreply.github.com>
2024-02-03 03:10:38 +0100

    fghj

--- application/routes.py
+++ application/routes.py
@@ -7,8 +7,27 @@ import json
 
 @app.route("/")
 def index():
-    entries = IncomeExpenses.query.order_by(IncomeExpenses.date.desc()).all()
-    return render_template("index.html", title="Index", entries=entries)
+    income_vs_expenses = db.session.query(db.func.sum(IncomeExpenses.amount), IncomeExpenses.type).group_by(
+        IncomeExpenses.type).order_by(IncomeExpenses.type).all()
+
+    dates = db.session.query(db.func.sum(IncomeExpenses.amount), IncomeExpenses.date).group_by(
+        IncomeExpenses.date).order_by(IncomeExpenses.date).all()
+
+    income_expense = []
+    for total_amount, _ in income_vs_expenses:
+        income_expense.append(total_amount)
+
+    over_time_expenditure = []
+    dates_labels = []
+    for amount, date in dates:
+        over_time_expenditure.append(amount)
+        dates_labels.append(date.strftime("%m-%d-%Y"))
+
+    return render_template("dashboard.html",
+                           income_vs_expenses=json.dumps(income_expense),
+                           over_time_expenditure=json.dumps(over_time_expenditure),
+                           dates_label=json.dumps(dates_labels))
+
 
 @app.route("/add", methods=["GET", "POST"])
 def add_expenses():
@@ -38,22 +57,9 @@ def delete(entry_id):
     flash("Deletion was success", 'success')
     return redirect(url_for("index"))
 
-@app.route('/dashboard')
-def dashboard():
-    income_vs_expenses = db.session.query(db.func.sum(IncomeExpenses.amount), IncomeExpenses.type).group_by(IncomeExpenses.type).order_by(IncomeExpenses.type).all()
+@app.route('/history')
+def history():
+    entries = IncomeExpenses.query.order_by(IncomeExpenses.date.desc()).all()
+    return render_template("index.html", title="Index", entries=entries)
 
-    dates = db.session.query(db.func.sum(IncomeExpenses.amount), IncomeExpenses.date).group_by(IncomeExpenses.date).order_by(IncomeExpenses.date).all()
-
-    income_expense = []
-    for total_amount, _ in income_vs_expenses:
-        income_expense.append(total_amount)
-
-    over_time_expenditure = []
-    dates_labels = []
-    for amount, date in dates:
-        over_time_expenditure.append(amount)
-        dates_labels.append(date.strftime("%m-%d-%Y"))
-
-    return render_template("dashboard.html",
-       income_vs_expenses=json.dumps(income_expense), over_time_expenditure = json.dumps(over_time_expenditure), dates_label=json.dumps(dates_labels))
 
